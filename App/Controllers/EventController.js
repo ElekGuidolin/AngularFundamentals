8ac64b9a48c698af075ae9684ca04428757d7af9
Elek Guidolin <elekguidolin@gmail.com>
2016-10-25 21:25:08 -0200

    Updated with more classes about filters and ngModel

--- App/Controllers/EventController.js
+++ App/Controllers/EventController.js
@@ -3,10 +3,16 @@
 eventsApp.controller('EventController',
     function EventController($scope) {
         $scope.snippet = '<span style="color:red">Hello World!</span>';
+        $scope.boolValue = true;
+        $scope.myStyle = { color: 'red' };
+        $scope.buttonDisabled = true;
+        $scope.myClass = "blue";
+        $scope.sortOrder = 'name';
         $scope.event = {
-            name: 'Elaine Beçudinha',
+            name: 'Angular Boot Camp',
             date: '23/10/2016',
             time: '18:00',
+            price: 34,
             location: {
                 address: 'Rua das Flores, 123',
                 city: 'São Paulo',
@@ -17,7 +23,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Bob Smith',
-                    duration: '1h',
+                    duration: 1,
                     level: 'Advanced',
                     abstract: 'In this session you will learn the ins and outs of directives.',
                     upVoteCount: 3
@@ -25,7 +31,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
-                    duration: '2h',
+                    duration: 2,
                     level: 'Basic',
                     abstract: 'This session will take a closer look to scopes.',
                     upVoteCount: 2
@@ -33,7 +39,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Great Guy',
-                    duration: '3h',
+                    duration: 4,
                     level: 'Medium',
                     abstract: 'Controllers are the beginning of everything Angular does.',
                     upVoteCount: 35
